02601bcd2cb565e2165a5ff4844a0915ac307c81
Salma Elshahawy <salmaeng71@gmail.com>
2020-11-16 14:57:33 -0500

    finish lab 6

--- Data_608/CUNY_DATA_608-master/module6/d3_lab/01answer.js
+++ Data_608/CUNY_DATA_608-master/module6/d3_lab/01answer.js
@@ -1,8 +1,24 @@
 d3.csv('ue_industry.csv', data => {
 
-    // Define your scales and generator here.
+    console.log(data);
 
-    d3.select('#answer1')
-        // append more elements here
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([1180, 20]);
+    
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([580, 20]);
 
+
+	var line = d3.line()
+				.x(d => xScale(+d.index))
+				.y(d => yScale(+d['Agriculture']))
+				.curve(d3.curveCardinal);
+                    
+	d3.select('#answer1')
+		.append('path')
+		.attr('d', line(data))
+		.attr('stroke', 'red')
+		
 });
