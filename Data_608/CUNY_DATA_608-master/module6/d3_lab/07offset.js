480c9a18a7eb63b2c9964bacac8651e167c030fa
Salma Elshahawy <salmaeng71@gmail.com>
2020-11-17 12:37:58 -0500

    finalize module6 data 608

--- Data_608/CUNY_DATA_608-master/module6/d3_lab/07offset.js
+++ Data_608/CUNY_DATA_608-master/module6/d3_lab/07offset.js
@@ -1,11 +1,11 @@
 d3.csv('ue_industry.csv', data => {
 
-    const industries = ['Agriculture','Business services','Construction','Education and Health',
-        'Finance','Government','Information','Leisure and hospitality','Manufacturing',
-        'Mining and Extraction','Other','Self-employed','Transportation and Utilities',
+    const industries = ['Agriculture', 'Business services', 'Construction', 'Education and Health',
+        'Finance', 'Government', 'Information', 'Leisure and hospitality', 'Manufacturing',
+        'Mining and Extraction', 'Other', 'Self-employed', 'Transportation and Utilities',
         'Wholesale and Retail Trade'];
 
-    const colors = ['#393b79', '#5253a3', '#6b6ecf', '#9c9ede', '#637939', '#8ca252', '#b5cf6b', 
+    const colors = ['#393b79', '#5253a3', '#6b6ecf', '#9c9ede', '#637939', '#8ca252', '#b5cf6b',
         '#cedb9c', '#8b6d31', '#bd9e38', '#e7ba52', '#e7cb93', '#843c39', '#ad494a'];
 
     const totalYmax = d3.sum(
@@ -17,7 +17,7 @@ d3.csv('ue_industry.csv', data => {
     const xScale = d3.scaleLinear()
         .domain(d3.extent(data, d => +d.index))
         .range([20, 1180]);
-    
+
     const yScale = d3.scaleLinear()
         .domain([0, totalYmax])
         .range([580, 20]);
@@ -29,7 +29,7 @@ d3.csv('ue_industry.csv', data => {
     const stackLayout = d3.stack()
         .keys(industries)
         .offset(d3.stackOffsetSilhouette);
-        
+
     const stackArea = d3.area()
         .x((d, i) => xScale(i))
         .y0(d => yScale(d[0]))
@@ -42,6 +42,7 @@ d3.csv('ue_industry.csv', data => {
         .enter().append('path')
         .attr('d', d => stackArea(d))
         .attr('class', d => 'path7')
+        .attr('transform', 'translate(100, -275)')
         .style('fill', d => fillScale(d.key));
 
 });
